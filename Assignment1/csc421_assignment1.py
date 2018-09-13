774f699de29affab1ca9a11456496530b436d457
Henri De Boever <henrideboever@gmail.com>
2018-09-12 21:43:52 -0700

    added class functions for WaterJug class

--- Assignment1/csc421_assignment1.py
+++ Assignment1/csc421_assignment1.py
@@ -12,33 +12,67 @@ from collections import OrderedDict
 # The goal state is when we have 1 liter left over in a jug
 class EnvState():
 
+	# Class initializer
 	def __init__(self):
 		print("Instantiating a new state")
 
-		# Track the water jug's contents in a state instance
-		water_jugs = []
+		# Track the water jug's contents in a state instance, self.water_jugs is a list that contains 3 jugs at each state
+		self.water_jugs = []
 
 # A water jug object is held within a state
-class WaterJug():
+class WaterJug(object):
 
 	# Class initializer
 	# The attributes below are specific to each instance of a WaterJug object
-	def __init__(self):
-
+	def __init__(self, name, capacity, num_gallons):
 		print("\nInstantiating a new WaterJug() object.\n")
-
-
-		# put relevant instance attributes here
+		self.name = name
+		self.max_capacity = capacity
+		self.num_gallons = num_gallons
 
 	# Class functions are defined below
+	def get_num_gallons(self):
+		print('\n%s has %s gallons in it.' % (self.name, self.num_gallons))
 
+	def pour_jug_on_groud(self):
+		if self.num_gallons >= 0:
+			self.num_gallons = 0
+		elif self.num_gallons == 0:
+			print("Cannot pour water from an empty jug")
 
+	def fill_from_tap(self):
+		# Filling jug from the faucet
+		if self.num_gallons < self.capacity:
+			self.num_gallons = self.capacity
+		else:
+			print("%s is already full" % self.name)
+
+	def transfer_water_to_other_jug(self, other_jug):
+		# Check if other jug is a jug object before proceeding
+		if isinstance(other_jug, WaterJug):
+			print("\nTransfering water from %s to %s" % (self.name, other_jug.name))
+			# Transfer
+			other_jug.num_gallons += self.num_gallons
+			# Catch the case of water overflow when pouring an amount larger than the capacity into the other jug
+			if other_jug.num_gallons > other_jug.capacity:
+				other_jug.num_gallons = other_jug.capacity
+
+		else:
+			print("Incorrect params")
+			sys.exit(0)
 
 # The main function serves as a test harness for the water jug class instances
 def main(argv):
 
-	print("Creating a new EnvState object")
-	print("Creating a new WaterJug object")
+	test_jug1 = WaterJug('12_gallon_jug', 12, 0)
+	test_jug2 = WaterJug('8_gallon_jug', 8, 0)
+	test_jug3 = WaterJug('3_gallon_jug', 3, 0)
+
+	test_jug1.get_num_gallons()
+	test_jug2.get_num_gallons()
+	test_jug3.get_num_gallons()
+
+	test_jug1.transfer_water_to_other_jug(test_jug2)
 
 
 if __name__ == "__main__":

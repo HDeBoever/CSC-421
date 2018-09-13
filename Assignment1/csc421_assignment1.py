7082e885f600cb5bc8a983c5fc269dcc44ee8be5
Henri De Boever <henrideboever@gmail.com>
2018-09-12 20:34:04 -0700

    First commit for CSC 421 Assignment 1

---
+++ Assignment1/csc421_assignment1.py
@@ -0,0 +1,27 @@
+'''
+CSC 421: Assignment 1
+@author : Henri De Boever / Alex Deweert
+'''
+
+
+class WaterJug():
+
+	# Class initializer
+	# The attributes below are specific to each instance of a WaterJug object
+	def __init__(self):
+		print("\nInstantiating a new WaterJug() object.\n")
+
+		# put relevant instance attributes here
+
+	# Class functions are defined below
+	
+
+
+# The main function serves as a test harness for the water jug class instances
+def main(argv):
+
+	print("Creating a new WaterJug object")
+
+
+if __name__ == "__main__":
+	main(sys.argv)

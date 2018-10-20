b07c063a7f4a45036ae462327e09a17bb5120a16
HDeBoever <henrideboever@gmail.com>
2018-10-19 22:25:27 -0700

    question 3 stuff

--- Assignment2/csp_post/CSPWaterZebra.java
+++ Assignment2/csp_post/CSPWaterZebra.java
@@ -7,66 +7,112 @@ import java.util.TreeSet;
 
 public class CSPWaterZebra extends CSP{
 
-	// Generate all the possible conditions given the input
-	public static ArrayList<ArrayList<String>> generateAllCombinations(String[] houses, String[] drinks, String[] nations, String[] pets, String[] cigs){
-		int num = 0;
-		//Cast all the hash sets
-
-		ArrayList<String> temp_string_array = new ArrayList<String>();
-		ArrayList<ArrayList<String>> final_string_array = new ArrayList<ArrayList<String>>();
-		for(String house : houses){
-			for(String drink : drinks){
-				for(String nation : nations){
-					for(String pet : pets){
-						for(String cig : cigs){
-
-							temp_string_array.add(house);
-							temp_string_array.add(drink);
-							temp_string_array.add(nation);
-							temp_string_array.add(pet);
-							temp_string_array.add(cig);
-
-							final_string_array.add(temp_string_array);
-
-							temp_string_array.clear();
-
-							System.out.println(house + " " + drink+ " " + nation + " "+ pet + " " + cig);
-							num += 1;
-						}
-					}
-				}
-			}
-			System.out.println();
-		}
-
-		System.out.println(num);
-
-		return final_string_array;
-	}
+	// // Generate all the possible conditions given the input
+	// public static ArrayList<ArrayList<String>> generateAllCombinations(String[] house_positions, String[] house_colours, String[] drinks, String[] nations, String[] pets, String[] cigs){
+	// 	int num = 0;
+	// 	//Cast all the hash sets
+	//
+	// 	ArrayList<String> temp_string_array = new ArrayList<String>();
+	// 	ArrayList<ArrayList<String>> final_string_array = new ArrayList<ArrayList<String>>();
+	//
+	// 	for(String position : house_positions){
+	// 		for(String house : house_colours){
+	// 			for(String drink : drinks){
+	// 				for(String nation : nations){
+	// 					for(String pet : pets){
+	// 						for(String cig : cigs){
+	//
+	// 							if(nation.equals("englishman") && house.equals("red")){
+	// 								temp_string_array.add(house);
+	// 								temp_string_array.add(drink);
+	// 								temp_string_array.add(nation);
+	// 								temp_string_array.add(pet);
+	// 								temp_string_array.add(cig);
+	//
+	// 								final_string_array.add(temp_string_array);
+	//
+	// 								temp_string_array.clear();
+	//
+	// 								System.out.println(position + " " + house + " " + drink+ " " + nation + " "+ pet + " " + cig);
+	// 								num += 1;
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 		System.out.println();
+	// 	}
+	//
+	// 	System.out.println(num);
+	//
+	// 	return final_string_array;
+	// }
 
 	public boolean isGood(Object X, Object Y, Object x, Object y){
 
+		System.out.print(X);
+		System.out.print(Y);
 
-		return false;
 	}
 
-
+	// TEST YOUR STUFF!
 	public static void main(String[] args){
 
-		String[] varColours = {"blue", "red", "yellow", "green", "ivory"};
+		//ArrayList<ArrayList<String>> combinations = generateAllCombinations(varHousePositions, varHouseColours, varDrink, varNationality, varPets, varFaggs);
 
-		String[] varDrink = {"coffee", "milk", "orange juice", "tea", "water"};
+		//Declare your variables!
+		String[] varHousePositions = {"pos1", "pos2", "pos3", "pos4", "pos5"};
+		String[] varHouseColours = {"colour1", "colour2", "colour3", "colour4", "colour5"};
+		String[] varDrink = {"drink1", "drink2", "drink3", "drink4", "drink5"};
+		String[] varNationality = {"nationality1", "nationality2", "nationality3", "nationality4", "nationality5"};
+		String[] varPet = {"pet1", "pet2", "pet3", "pet4", "pet5"};
+		String[] varCig = {"cig1", "cig2", "cig3", "cig4", "cig5"};
 
-		String[] varNationality = {"Englishman", "Spaniard", "Norwegian", "Ukranian", "Japanese"};
+		//Declare your domains!
+		String[] domainPosition = {"1", "2", "3", "4", "5"};
+		String[] domainHouseColours = {"blue", "red", "yellow", "green", "ivory"};
+		String[] domainDrink = {"coffee", "milk", "orange juice", "tea", "water"};
+		String[] domainNationality = {"englishman", "spaniard", "norwegian", "ukranian", "japanese"};
+		String[] domainPets = {"horse", "fox", "dog", "snail", "zebra"};
+		String[] domainCigs = {"chesterfields", "kools", "lucky-strike", "old old", "parliament"};
 
-		String[] varPets = {"horse", "fox", "dog", "snail", "zebra"};
 
-		String[] varFaggs = {"Chesterfields", "Kools", "Lucky-Strike", "Old Gold", "Parliament"};
+		CSPGraphColoring csp = new CSPWaterZebraColoring();
 
-		ArrayList<ArrayList<String>> combinations = generateAllCombinations(varColours, varDrink, varNationality, varPets, varFaggs);
+		for(Object X : varDrink)
+			csp.addDomain(X, domainDrink);
+		for(Object X : varNationality)
+			csp.addDomain(X, domainNationality);
+		for(Object X : varPet)
+			csp.addDomain(X, domainPets);
+		for(Object X : varCig)
+			csp.addDomain(X, domainCigs);
+		for(Object X : varHouseColours)
+			csp.addDomain(X, domainHouseColours);
 
+		// Add all pair arcs
+		csp.addBidirectionalArc("englishman", "red");
+		csp.addBidirectionalArc("spaniard", "dog");
+		csp.addBidirectionalArc("green", "coffee");
+		csp.addBidirectionalArc("ukranian", "tea");
+		// csp.addBidirectionalArc("green", "ivory house next to")???;
+		csp.addBidirectionalArc("snails", "old gold");
+		csp.addBidirectionalArc("yellow", "kools");
+		csp.addBidirectionalArc("3", "milk");
+		csp.addBidirectionalArc("1", "Norwegian");
+		csp.addBidirectionalArc("chesterfields", "fox");
+		// csp.addBidirectionalArc("horse", "kools"); ?????
+		csp.addBidirectionalArc("lucky strike", "orange juice");
+		csp.addBidirectionalArc("japanese", "parliament");
+		csp.addBidirectionalArc("norwegian", "blue");
 
+		// // To do after we come up with our constraints
+		// for(Object[] p : pairs)
+		// 	csp.addBidirectionalArc(p[0], p[1]);
 
+		Search search = new Search(csp);
+		System.out.println(search.BacktrackingSearch());
 
 	}
 }
